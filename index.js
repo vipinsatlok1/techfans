2c7ff54222f8ee18c4f0ba243272083d8ccce9d6
vipinsatlok1 <126874481+vipinsatlok1@users.noreply.github.com>
2023-03-05 23:39:16 +0000

    save

--- index.js
+++ index.js
@@ -5,6 +5,39 @@ const port = process.env.PORT || 5000
 const passport = require("./passport")
 const session = require("express-session")
 const cookieParser = require('cookie-parser');
+const multer = require('multer')
+const { youtubeThumbnailDownload } = require("techfans-tools")
+
+app.set('view engine', 'ejs');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '')
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+        cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+})
+
+app.use(express.json())
+app.use(express.urlencoded({extended : false}))
+
+
+app.post("/thumbnail", async (req, res) => {
+    console.log(req.body)
+    await youtubeThumbnailDownload(req.body.url, "thumbnail.jpg")
+    res.send("ok")
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/file', upload.single('file'), function (req, res) {
+    // req.file is the name of your file in the form above, here 'uploaded_file'
+    // req.body will hold the text fields, if there were any 
+    console.log(req.file, req.body)
+});
 
 app.use(cookieParser());
 
@@ -23,9 +56,10 @@ function isLoggedIn(req, res, next) {
     }
 }
 
-app.get('/', isLoggedIn, function (req, res) {
-    res.sendFile(__dirname + "/index.html")
+app.get('/', (req, res) => {
+    res.render('pages/home', { title: 'Home' });
 });
+
 
 app.get('/login', function (req, res) {
     if (req.cookies?.user) return res.redirect("/")
