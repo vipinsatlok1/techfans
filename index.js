896db3dcf9080d2e5d73d4ec7597a382baf1b4fa
vipinsatlok1 <126874481+vipinsatlok1@users.noreply.github.com>
2023-03-05 14:25:27 +0000

    adding login to google

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 5000
+const passport = require("./passport")
+const session = require("express-session")
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+function isLoggedIn(req, res, next) {
+
+    if (req.cookies?.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+app.get('/', isLoggedIn, function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.get('/login', function (req, res) {
+    if (req.cookies?.user) return res.redirect("/")
+    res.sendFile(__dirname + "/login.html")
+});
+
+
+app.get('/login/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/login/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+        res.cookie('user', req.user);
+        res.redirect('/');
+    });
+
+app.get('/logout', function (req, res) {
+    res.clearCookie("user")
+    res.redirect("/login")
+});
 
 
 app.listen(port, () => console.log(`server start at port 5000`))
---
+++ passport/index.js
@@ -0,0 +1,25 @@
+const passport = require("passport")
+const GoogleStrategy = require("passport-google-oauth20").Strategy
+const fs = require("fs")
+
+const secretData = {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: process.env.CALLBACK_URL
+}
+
+const getUserData = (accessToken, refreshToken, profile, done) => {
+    return done(null, profile);
+}
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy(secretData, getUserData));
+
+module.exports = passport
