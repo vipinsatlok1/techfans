531aaa40433ff95a9c0139e143f63af09c2c49d5
Vipin KUmar <126874481+vipinsatlok1@users.noreply.github.com>
2023-03-06 04:13:03 +0000

    sasa

--- src/services/youtubeThumbnail.js
+++ src/services/youtubeThumbnail.js
@@ -1,13 +1,14 @@
 const fs = require("fs")
 const download = require("image-downloader")
 const grabLink = require('youtube-thumbnail-grabber')
+const path = require("path")
 
 const youtubeThumbnailDownload = async (req, res) => {
     // get youtube image url
     const url = grabLink(req.body.url, 'max')
 
     // set destination where save image
-    const dest = __dirname + "/example.jpg"
+    const dest = path.resolve(__dirname, "../../", "example.jpg")
 
     await download.image({ url, dest })
     res.download(dest, "download.jpg")
